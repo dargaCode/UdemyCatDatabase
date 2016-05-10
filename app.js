69d7709a8cb2379f6066e22a304ff98a36de34fa
dargaCode <darga.code@gmail.com>
2016-05-09 22:55:49 -0700

    Add schema

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ const mongoose = require('mongoose');
 // SETTINGS
 
 mongoose.connect('mongodb://localhost/cat_app');
+
+// CONSTANTS
+
+const catSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  breed: String,
+  color: String
+});
+
+const Cat = mongoose.model('Cat', catSchema);
+
